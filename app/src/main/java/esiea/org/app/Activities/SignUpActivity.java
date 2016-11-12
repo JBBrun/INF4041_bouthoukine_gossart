84505cce6b6ac8e608557dd35188205a654d03c6
Ayoub <bouthoukine.ayoub@gmail.com>
2016-11-12 01:35:15 +0100

    SignUp design

--- app/src/main/java/esiea/org/app/Activities/SignUpActivity.java
+++ app/src/main/java/esiea/org/app/Activities/SignUpActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import esiea.org.app.Database.DatabaseHandler;
@@ -24,6 +25,9 @@ public class SignUpActivity extends Activity {
     EditText nameView ;
     EditText emailView ;
     EditText passwordView;
+    RadioButton clientView;
+    RadioButton consultantView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,8 @@ public class SignUpActivity extends Activity {
         nameView = (EditText) findViewById(R.id.nameValue);
         emailView = (EditText) findViewById(R.id.emailValue);
         passwordView = (EditText) findViewById(R.id.passValue);
+        clientView = (RadioButton) findViewById(R.id.radioCLient);
+        consultantView = (RadioButton) findViewById(R.id.radioCOnsultant);
 
     }
 
@@ -50,12 +56,15 @@ public class SignUpActivity extends Activity {
         String name = nameView.getText().toString();
         String email = emailView.getText().toString();
         String password = passwordView.getText().toString();
+        int profil = clientView.isChecked() ? 0 : 1;
 
-        User user = new User(name,email,password,2);
+        User user = new User(name,email,password,profil);
         db.createUser(user);
         Toast.makeText(this,R.string.registration_success,Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this,SignInActivity.class);
         startActivity(intent);
         finish();
     }
+
+
 }
